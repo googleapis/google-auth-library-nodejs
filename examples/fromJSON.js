386b47dd8f6838ba9e69ad55c93e16af11e9fdd1
Justin Beckwith <beckwith@google.com>
2018-01-06 14:21:26 -0800

    fix busted formatting

--- examples/fromJSON.js
+++ examples/fromJSON.js
@@ -33,12 +33,10 @@ const { GoogleAuth } = require('../build/src/index');
  *       "client_id": "your-client-id",
  *       "auth_uri": "https://accounts.google.com/o/oauth2/auth",
  *       "token_uri": "https://accounts.google.com/o/oauth2/token",
- *       "auth_provider_x509_cert_url":
- *"https://www.googleapis.com/oauth2/v1/certs", "client_x509_cert_url":
- *"your-cert-url"
+ *       "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+ *       "client_x509_cert_url": "your-cert-url"
  *     }'
  * $ node fromJSON.js
- *
  **/
 
 const keysEnvVar = process.env['CREDS'];
