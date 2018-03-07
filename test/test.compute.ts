9d81fcef8d0d61cd6777a721a881429b0a23ba61
Justin Beckwith <justin.beckwith@gmail.com>
2018-03-07 12:20:52 -0800

    fix: limit situations where we retry (#307)

--- test/test.compute.ts
+++ test/test.compute.ts
@@ -141,11 +141,10 @@ it('should return false for createScopedRequired', () => {
 });
 
 it('should return a helpful message on request response.statusCode 403', async () => {
-  // Mock the credentials object.
+  // Mock the credentials object.  Make sure there's no expiry_date set.
   compute.credentials = {
     refresh_token: 'hello',
     access_token: 'goodbye',
-    expiry_date: (new Date(9999, 1, 1)).getTime()
   };
 
   const scopes = [
