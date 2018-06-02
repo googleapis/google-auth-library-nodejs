44bfb70031d126e6c42391c9ddc24783c7fdee82
Alexander Fenster <fenster@google.com>
2018-06-01 22:11:52 -0700

    async/await ftw

--- src/crypto/browser/crypto.ts
+++ src/crypto/browser/crypto.ts
@@ -23,14 +23,10 @@ import * as TextEncoding from 'text-encoding-shim';
 import {Crypto, JwkCertificate} from '../crypto';
 
 export class BrowserCrypto implements Crypto {
-  sha256DigestBase64(str: string): Promise<string> {
-    return new Promise(resolve => {
-      window.crypto.subtle
-          .digest('SHA-256', new TextEncoding.TextEncoder().encode(str))
-          .then(
-              arrayBuffer =>
-                  resolve(base64js.fromByteArray(new Uint8Array(arrayBuffer))));
-    });
+  async sha256DigestBase64(str: string): Promise<string> {
+    const arrayBuffer = await window.crypto.subtle.digest(
+        'SHA-256', new TextEncoding.TextEncoder().encode(str));
+    return base64js.fromByteArray(new Uint8Array(arrayBuffer));
   }
 
   randomBytesBase64(count: number): string {
@@ -39,24 +35,22 @@ export class BrowserCrypto implements Crypto {
     return base64js.fromByteArray(array);
   }
 
-  verify(pubkey: JwkCertificate, data: string, signature: string):
+  async verify(pubkey: JwkCertificate, data: string, signature: string):
       Promise<boolean> {
-    return new Promise(resolve => {
-      const algo = {
-        name: 'RSASSA-PKCS1-v1_5',
-        hash: {name: 'SHA-256'},
-      };
-      const dataArray = new TextEncoding.TextEncoder().encode(data);
-      // base64js requires padding, so let's add some '='
-      while (signature.length % 4 !== 0) {
-        signature += '=';
-      }
-      const signatureArray = base64js.toByteArray(signature);
-      window.crypto.subtle.importKey('jwk', pubkey, algo, true, ['verify'])
-          .then(ck => {
-            window.crypto.subtle.verify(algo, ck, signatureArray, dataArray)
-                .then(resolve);
-          });
-    });
+    const algo = {
+      name: 'RSASSA-PKCS1-v1_5',
+      hash: {name: 'SHA-256'},
+    };
+    const dataArray = new TextEncoding.TextEncoder().encode(data);
+    // base64js requires padding, so let's add some '='
+    while (signature.length % 4 !== 0) {
+      signature += '=';
+    }
+    const signatureArray = base64js.toByteArray(signature);
+    const cryptoKey = await window.crypto.subtle.importKey(
+        'jwk', pubkey, algo, true, ['verify']);
+    const result = await window.crypto.subtle.verify(
+        algo, cryptoKey, signatureArray, dataArray);
+    return result;
   }
 }
--- src/crypto/node/crypto.ts
+++ src/crypto/node/crypto.ts
@@ -18,19 +18,18 @@ import * as crypto from 'crypto';
 import {Crypto} from '../crypto';
 
 export class NodeCrypto implements Crypto {
-  sha256DigestBase64(str: string): Promise<string> {
-    return Promise.resolve(
-        crypto.createHash('sha256').update(str).digest('base64'));
+  async sha256DigestBase64(str: string): Promise<string> {
+    return crypto.createHash('sha256').update(str).digest('base64');
   }
 
   randomBytesBase64(count: number): string {
     return crypto.randomBytes(count).toString('base64');
   }
 
-  verify(pubkey: string, data: string|Buffer, signature: string):
+  async verify(pubkey: string, data: string|Buffer, signature: string):
       Promise<boolean> {
     const verifier = crypto.createVerify('sha256');
     verifier.update(data);
-    return Promise.resolve(verifier.verify(pubkey, signature, 'base64'));
+    return verifier.verify(pubkey, signature, 'base64');
   }
 }
