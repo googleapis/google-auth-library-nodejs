fb74b9c509aa3de3a93ab9f4bc471606c4dfd666
Justin Beckwith <justin.beckwith@gmail.com>
2018-08-28 08:38:15 -0700

    run synth again

--- synth.py
+++ synth.py
@@ -1,12 +1,7 @@
 import synthtool as s
 import synthtool.gcp as gcp
 import logging
-import subprocess
-
 logging.basicConfig(level=logging.DEBUG)
 common_templates = gcp.CommonTemplates()
-templates = common_templates.node_library(
-  package_name="google-auth-library",
-  repo_name="google/google-auth-library-nodejs"
-)
+templates = common_templates.node_library()
 s.copy(templates)
