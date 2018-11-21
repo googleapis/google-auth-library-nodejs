f5e47f8c4745b9810ac09f027f891ed5097c455a
Justin Beckwith <justin.beckwith@gmail.com>
2018-11-21 13:09:59 -0800

    refactor: drop non-required modules (#542)

--- system-test/test.kitchen.ts
+++ system-test/test.kitchen.ts
@@ -17,11 +17,11 @@
 import * as cp from 'child_process';
 import * as mv from 'mv';
 import {ncp} from 'ncp';
-import * as pify from 'pify';
 import * as tmp from 'tmp';
+import {promisify} from 'util';
 
-const mvp = pify(mv);
-const ncpp = pify(ncp);
+const mvp = promisify(mv) as {} as (...args: string[]) => Promise<void>;
+const ncpp = promisify(ncp);
 const keep = !!process.env.GALN_KEEP_TEMPDIRS;
 const stagingDir = tmp.dirSync({keep, unsafeCleanup: true});
 const stagingPath = stagingDir.name;
