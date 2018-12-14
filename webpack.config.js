fb400459659d70d74adbe2e8f1b1b0a07cb2c6a2
Alexander Fenster <fenster@google.com>
2018-12-14 14:33:08 -0800

    fixing webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
 const path = require('path');
-const webpack = require('webpack');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: './src/index.ts',
   resolve: {
-    extensions: [ '.ts', '.js', '.json' ],
-		alias: {
-			'../../package.json': path.resolve(__dirname, 'package.json')
-		}
+    extensions: ['.ts', '.js', '.json'],
+    alias: {
+      '../../package.json': path.resolve(__dirname, 'package.json'),
+    },
   },
   output: {
     library: 'GoogleAuth',
     filename: 'google-auth-library.min.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   node: {
-    'child_process': 'empty',
-    'fs': 'empty',
-    'crypto': 'empty'
+    child_process: 'empty',
+    fs: 'empty',
+    crypto: 'empty',
   },
   module: {
     rules: [
       {
         test: /node_modules\/gtoken\//,
-        use: 'null-loader'
+        use: 'null-loader',
       },
       {
         test: /node_modules\/pkginfo\//,
-        use: 'null-loader'
+        use: 'null-loader',
+      },
+      {
+        test: /node_modules\/https-proxy-agent\//,
+        use: 'null-loader',
       },
       {
         test: /\.ts$/,
         use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+    ],
   },
   mode: 'production',
-	plugins: [
-//    new BundleAnalyzerPlugin()  // TBD: to be removed before merge
-  ]
+  plugins: [],
 };
