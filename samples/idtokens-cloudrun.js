15bfae2c1c639239e23a5b4f2f6dacaf9e3f5673
Aditya Thakur <aditya.thakur.261099@gmail.com>
2020-07-12 13:06:24 +0530

    feat: Added option for processing POST requests
    Options for POST request added alongwith some readability improvements.

--- samples/idtokens-cloudrun.js
+++ samples/idtokens-cloudrun.js
@@ -11,36 +11,57 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// sample-metadata:
-//   title: ID Tokens for Cloud Run
-//   description: Requests a Cloud Run URL with an ID Token.
-//   usage: node idtokens-cloudrun.js <url> [<target-audience>]
+/*
+sample-metadata:
+  title       : ID Tokens for Cloud Run
+  description : Requests a Cloud Run URL with an ID Token.
+  usage       : node idtokens-cloudrun.js <url> [<target-audience>] <method> [<data>]
+*/
+
+//NOTE: 'data' is an optional parameter and will be passed only
+//if needed by the request method as the request body.
 
 'use strict';
 
+/* TODO(developer):
+  - Pass parameters as per your requirements. (Description below)
+  - Parameters for Cloud Run endpoint:
+    1. url             : URL for your Cloud Run Application
+    2. targetAudience  : Your Target Audience
+    3. method          : Request Method => GET | POST | ...
+    4. data            : Request Body (as per requirement)
+*/
+
+// Modify default parameters as per your requirements (if needed)
 function main(
   url = 'https://service-1234-uc.a.run.app',
-  targetAudience = null
+  targetAudience = null,
+  method = 'GET',
+  data
 ) {
-  // [START google_auth_idtoken_cloudrun]
-  /**
-   * TODO(developer): Uncomment these variables before running the sample.
-   */
-  // const url = 'https://YOUR_CLOUD_RUN_URL.run.app';
+  // Google Auth Library Initialization
   const {GoogleAuth} = require('google-auth-library');
   const auth = new GoogleAuth();
 
   async function request() {
     if (!targetAudience) {
-      // Use the request URL hostname as the target audience for Cloud Run requests
+      // Using request URL hostname as the target audience for Cloud Run requests
       const {URL} = require('url');
       targetAudience = new URL(url).origin;
     }
     console.info(
       `request Cloud Run ${url} with target audience ${targetAudience}`
     );
+    // Fetch access token and use it for authenticated Cloud Run requests.
     const client = await auth.getIdTokenClient(targetAudience);
-    const res = await client.request({url});
+
+    let res;
+    if (method === 'GET') {
+      res = await client.request({url});
+    } else if (method === 'POST') {
+      res = await client.request({url, data, method});
+    }
+
     console.info(res.data);
   }
 
