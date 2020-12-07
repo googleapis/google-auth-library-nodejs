4435bb6287e6dc8ab2b9d63ce89598710a3a6763
Sofia Leon <sofialeon@google.com>
2020-12-07 15:50:56 -0800

    fix(deps); upgradae webpack

--- samples/verifyIdToken-iap.js
+++ samples/verifyIdToken-iap.js
@@ -18,7 +18,10 @@
 
 'use strict';
 
+const { create } = require('domain');
 const {OAuth2Client} = require('google-auth-library');
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
 
 /**
  * Verify the ID token from IAP
@@ -36,20 +39,31 @@ function main(
    */
   // const iapJwt = 'SOME_ID_TOKEN'; // JWT from the "x-goog-iap-jwt-assertion" header
 
-  let expectedAudience = null;
-  if (projectNumber && projectId) {
-    // Expected Audience for App Engine.
-    expectedAudience = `/projects/${projectNumber}/apps/${projectId}`;
-  } else if (projectNumber && backendServiceId) {
-    // Expected Audience for Compute Engine
-    expectedAudience = `/projects/${projectNumber}/global/backendServices/${backendServiceId}`;
-  }
+  // function createJWT() {
+    // Create a JWT to authenticate this device. The device will be disconnected
+    // after the token expires, and will have to reconnect with a new token. The
+    // audience field should always be set to the GCP project id.
+  //   const token = {
+  //     iat: parseInt(Date.now() / 1000),
+  //     exp: parseInt(Date.now() / 1000) + 40 * 60, // 20 minutes
+  //     aud: 'coleleah-sofialeon',
+  //   };
+  //   const privateKey = fs.readFileSync('/Users/sofialeon/gcp/google-auth-library-nodejs/samples/ec_private_testSample.pem');
+  //   jwt.sign(token, privateKey, { algorithm: 'ES256' });
+  //   console.log(jwt);
+  //   return jwt.decode;
+  // };
+  //iapJwt = createJWT();
+iapJwt = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjJlMzAyNWYyNmI1OTVmOTZlYWM5MDdjYzJiOTQ3MTQyMmJjYWViOTMiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiIvcHJvamVjdHMvMjA0NTM1MTk4NzA2L2FwcHMvY29sZWxlYWgtc29maWFsZW9uIiwiYXpwIjoidGVzdC1hY2NvdW50QGNvbGVsZWFoLXNvZmlhbGVvbi5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsImVtYWlsIjoidGVzdC1hY2NvdW50QGNvbGVsZWFoLXNvZmlhbGVvbi5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJleHAiOjE2MDY4NTQ1NTAsImlhdCI6MTYwNjg1MDk1MCwiaXNzIjoiaHR0cHM6Ly9hY2NvdW50cy5nb29nbGUuY29tIiwic3ViIjoiMTA0MTg4NDQyMjkzMjgyOTU0OTQ0In0.ncPa_rRA-_DE6hgFBeP9SIRxyt-p4vgUd2iShyN116tR9_xZTlRmZLNO15EtG8QqgJn9FvSBRadzD_cXozVjWUsG5DGAtgqbr2n2LWw4hu4Lq9vStz6eEYUo_PdQ76FW850ZPX9qWiUYPgMmQwgxhaXD4qecDaV7L5HBmoEqdfCfwbRw-XRnA1483aAiH-HhsK3PcitgFLUSHmq5Nw8a6Moj7ABXyRHOncqUrSUhYELzXHgwZY3a6EqN7d_CvJp4l_BiJKpeJQRm8UdmpGDhl2Cb563SkTsGtuO7uaEr-v1awQt6Gh-QQ1oVUNsTrnDCn4MlurhKW02OfdAffkSFGg';
+  let expectedAudience = `/projects/204535198706/apps/coleleah-sofialeon`;
 
-  const oAuth2Client = new OAuth2Client();
+
+  const oAuth2Client = new OAuth2Client("204535198706-a44qeclp37upo4oedm3p46ksckt6k587.apps.googleusercontent.com", "3Y7PWUhBBFYwYDrj2CcGKxaG", "http://localhost:3000");
 
   async function verify() {
     // Verify the id_token, and access the claims.
     const response = await oAuth2Client.getIapPublicKeys();
+    console.log('RESPONSE '+JSON.stringify(response.pubkeys));
     const ticket = await oAuth2Client.verifySignedJwtWithCertsAsync(
       iapJwt,
       response.pubkeys,
