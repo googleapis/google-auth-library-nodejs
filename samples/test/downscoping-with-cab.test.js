924f67ea5eee8ae4f93fba612560bde5d8721067
Daniel Bankhead <danielbankhead@google.com>
2022-02-15 12:05:37 -0800

    chore: Log `output` to debug sample test error on Kokoro

--- samples/test/downscoping-with-cab.test.js
+++ samples/test/downscoping-with-cab.test.js
@@ -30,9 +30,9 @@ const {promisify} = require('util');
 const exec = promisify(cp.exec);
 // Copy values from the output of samples/scripts/downscoping-with-cab-setup.js.
 // GCS bucket name.
-const bucketName = 'cab-int-bucket-z2zsauf4sj';
+const bucketName = 'danielbankhead-gcp-playground-test';
 // GCS object name.
-const objectName = 'cab-first-z2zsauf4sj.txt';
+const objectName = '20MB.zip';
 
 /**
  * Runs the provided command using asynchronous child_process.exec.
@@ -59,6 +59,8 @@ describe('samples for downscoping with cab', () => {
         OBJECT_NAME: objectName,
       },
     });
+
+    console.log(output);
 
     // Confirm expected script output.
     assert.match(output, /Successfully retrieved file/);
