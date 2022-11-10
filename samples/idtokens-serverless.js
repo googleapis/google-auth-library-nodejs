08ac4ddf98c8cc20ac5687fb4320220270082a03
Patti Shin <pattishin@users.noreply.github.com>
2022-11-09 17:14:47 -0800

    refactor: updating to clarify audience + url example usage

--- samples/idtokens-serverless.js
+++ samples/idtokens-serverless.js
@@ -45,13 +45,11 @@ function main(
   // [END cloudrun_service_to_service_auth]
 
   // [START functions_bearer_token]
-  // For Cloud Functions, `endpoint` and `audience` should be equal.
-
-  // Example: https://project-region-projectid.cloudfunctions.net/myFunction
-  // const url = 'https://TARGET_HOSTNAME/TARGET_URL';
-
-  // Example (Cloud Functions): https://project-region-projectid.cloudfunctions.net/myFunction
-  // const targetAudience = 'https://TARGET_AUDIENCE/';
+  
+  // Cloud Functions uses your function's url as the `targetAudience` value
+  // const targetAudience = 'https://project-region-projectid.cloudfunctions.net/myFunction';
+  // For Cloud Functions, endpoint (`url`) and `targetAudience` should be equal
+  
   // [END functions_bearer_token]
 
   // [START functions_bearer_token]
