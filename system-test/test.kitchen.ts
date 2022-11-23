c8a68022f25ba9a6d794541805b7c180831b1af0
Daniel Bankhead <danielbankhead@google.com>
2022-11-23 15:13:18 -0800

    test: Use Native FS Temp Directory Tools
    Fixes: #1481

--- system-test/test.kitchen.ts
+++ system-test/test.kitchen.ts
@@ -18,8 +18,8 @@ import * as execa from 'execa';
 import * as fs from 'fs';
 import * as mv from 'mv';
 import {ncp} from 'ncp';
+import * as os from 'os';
 import * as path from 'path';
-import * as tmp from 'tmp';
 import {promisify} from 'util';
 
 const mvp = promisify(mv) as {} as (...args: string[]) => Promise<void>;
@@ -28,18 +28,19 @@ const keep = !!process.env.GALN_KEEP_TEMPDIRS;
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const pkg = require('../../package.json');
 
-let stagingDir: tmp.DirResult;
-let stagingPath: string;
+let stagingDir: string;
 async function packAndInstall() {
-  stagingDir = tmp.dirSync({keep, unsafeCleanup: true});
-  stagingPath = stagingDir.name;
+  stagingDir = await fs.promises.mkdtemp(
+    path.join(os.tmpdir(), 'google-auth-library-nodejs-pack-')
+  );
+
   await execa('npm', ['pack'], {stdio: 'inherit'});
   const tarball = `${pkg.name}-${pkg.version}.tgz`;
   // stagingPath can be on another filesystem so fs.rename() will fail
   // with EXDEV, hence we use `mv` module here.
-  await mvp(tarball, `${stagingPath}/google-auth-library.tgz`);
-  await ncpp('system-test/fixtures/kitchen', `${stagingPath}/`);
-  await execa('npm', ['install'], {cwd: `${stagingPath}/`, stdio: 'inherit'});
+  await mvp(tarball, `${stagingDir}/google-auth-library.tgz`);
+  await ncpp('system-test/fixtures/kitchen', `${stagingDir}/`);
+  await execa('npm', ['install'], {cwd: `${stagingDir}/`, stdio: 'inherit'});
 }
 
 describe('pack and install', () => {
@@ -60,8 +61,8 @@ describe('pack and install', () => {
     this.timeout(40000);
     await packAndInstall();
     // we expect npm install is executed in the before hook
-    await execa('npx', ['webpack'], {cwd: `${stagingPath}/`, stdio: 'inherit'});
-    const bundle = path.join(stagingPath, 'dist', 'bundle.min.js');
+    await execa('npx', ['webpack'], {cwd: `${stagingDir}/`, stdio: 'inherit'});
+    const bundle = path.join(stagingDir, 'dist', 'bundle.min.js');
     const stat = fs.statSync(bundle);
     assert(stat.size < 256 * 1024);
   });
@@ -69,9 +70,21 @@ describe('pack and install', () => {
   /**
    * CLEAN UP - remove the staging directory when done.
    */
-  afterEach('cleanup staging', () => {
+  afterEach('cleanup staging', async () => {
     if (!keep) {
-      stagingDir.removeCallback();
+      if ('rm' in fs.promises) {
+        await fs.promises.rm(stagingDir, {
+          force: true,
+          recursive: true,
+        });
+      } else {
+        // Must be on Node 14-.
+        // Here, `rmdir` can also delete files.
+        // Background: https://github.com/nodejs/node/issues/34278
+        await fs.promises.rmdir(stagingDir, {
+          recursive: true,
+        });
+      }
     }
   });
 });
