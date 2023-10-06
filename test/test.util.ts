311d6a1e31a0e4a875f8d1138ba45d654db6af5f
Daniel Bankhead <danielbankhead@google.com>
2023-10-06 16:20:00 -0700

    refactor: Replace LRU Dependency With Custom Implementation (#1659)
    * build: remove `lru-cache`
    
    * feat: Add Custom LRU
    
    * fix: remove log
    
    * refactor: Use `Map` instead of Linked List
    
    * chore: Move `LRU` to a `util.ts`
    
    * chore: Rename for clarity
    
    * test: Add `LRUCache` tests
    
    * chore: use utility LRUCache
    
    * fix: use manual Promise `setTimeout`
    
    'timers/promises' is not available in Node 14
    
    * chore: lint
    
    * docs: LRUCache

---
+++ test/test.util.ts
@@ -0,0 +1,67 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {strict as assert} from 'assert';
+
+import {LRUCache} from '../src/util';
+
+describe('util', () => {
+  describe('LRUCache', () => {
+    it('should set and get a cached item', () => {
+      const expected = 'value';
+      const lru = new LRUCache({capacity: 5});
+      lru.set('sample', expected);
+
+      assert.equal(lru.get('sample'), expected);
+    });
+
+    it('should evict oldest items when over capacity', () => {
+      const capacity = 5;
+      const overCapacity = 2;
+
+      const lru = new LRUCache({capacity});
+
+      for (let i = 0; i < capacity + overCapacity; i++) {
+        lru.set(`${i}`, i);
+      }
+
+      // the first few shouldn't be there
+      for (let i = 0; i < overCapacity; i++) {
+        assert.equal(lru.get(`${i}`), undefined);
+      }
+
+      // the rest should be there
+      for (let i = overCapacity; i < capacity + overCapacity; i++) {
+        assert.equal(lru.get(`${i}`), i);
+      }
+    });
+
+    it('should evict items older than a supplied `maxAge`', async () => {
+      const maxAge = 50;
+
+      const lru = new LRUCache({capacity: 5, maxAge});
+
+      lru.set('first', 1);
+      lru.set('second', 2);
+
+      await new Promise(res => setTimeout(res, maxAge + 1));
+
+      lru.set('third', 3);
+
+      assert.equal(lru.get('first'), undefined);
+      assert.equal(lru.get('second'), undefined);
+      assert.equal(lru.get('third'), 3);
+    });
+  });
+});
