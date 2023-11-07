4ef5baf77ee44ba4fbe9a4718cf35a3a65aedf9e
Dhoni77 <aldrinmarz7@gmail.com>
2023-11-07 15:14:28 +0530

    refactor: Remove Transporter and DefaultTransporter from GoogleAuth class

--- src/auth/googleauth.ts
+++ src/auth/googleauth.ts
@@ -21,8 +21,6 @@ import * as path from 'path';
 import * as stream from 'stream';
 
 import {Crypto, createCrypto} from '../crypto/crypto';
-import {DefaultTransporter, Transporter} from '../transporters';
-
 import {Compute, ComputeOptions} from './computeclient';
 import {CredentialBody, ImpersonatedJWTInput, JWTInput} from './credentials';
 import {IdTokenClient} from './idtokenclient';
@@ -134,8 +132,6 @@ const GoogleAuthExceptionMessages = {
 } as const;
 
 export class GoogleAuth<T extends AuthClient = JSONClient> {
-  transporter?: Transporter;
-
   /**
    * Caches a value indicating whether the auth layer is running on Google
    * Compute Engine.
@@ -167,11 +163,6 @@ export class GoogleAuth<T extends AuthClient = JSONClient> {
   private keyFilename?: string;
   private scopes?: string | string[];
   private clientOptions?: AuthClientOptions;
-
-  /**
-   * Export DefaultTransporter as a static property of the class.
-   */
-  static DefaultTransporter = DefaultTransporter;
 
   /**
    * Configuration is resolved in the following order of precedence:
