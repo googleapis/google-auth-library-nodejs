468367d7b31fa1c95f1ca6072cd54b9e59bf9f48
aeitzman <12433791+aeitzman@users.noreply.github.com>
2024-04-03 15:45:03 -0700

    feat: adds support for creating AWS and Identity Pool credentials with custom suppliers. (#1783)
    * feat: adds support for users to build credentials with custom suppliers
    
    Also adds default values to make it easier to instantiate credentials in code.
    
    * Apply suggestions from code review
    
    Co-authored-by: Leo <39062083+lsirac@users.noreply.github.com>
    
    * responding to review comments
    
    ---------
    
    Co-authored-by: Leo <39062083+lsirac@users.noreply.github.com>

--- src/auth/externalAccountAuthorizedUserClient.ts
+++ src/auth/externalAccountAuthorizedUserClient.ts
@@ -39,6 +39,7 @@ import {
  */
 export const EXTERNAL_ACCOUNT_AUTHORIZED_USER_TYPE =
   'external_account_authorized_user';
+const DEFAULT_TOKEN_URL = 'https://sts.{universeDomain}/v1/oauthtoken';
 
 /**
  * External Account Authorized User Credentials JSON interface.
@@ -171,6 +172,9 @@ export class ExternalAccountAuthorizedUserClient extends AuthClient {
     additionalOptions?: AuthClientOptions
   ) {
     super({...options, ...additionalOptions});
+    if (options.universe_domain) {
+      this.universeDomain = options.universe_domain;
+    }
     this.refreshToken = options.refresh_token;
     const clientAuth = {
       confidentialClientType: 'basic',
@@ -179,7 +183,8 @@ export class ExternalAccountAuthorizedUserClient extends AuthClient {
     } as ClientAuthentication;
     this.externalAccountAuthorizedUserHandler =
       new ExternalAccountAuthorizedUserHandler(
-        options.token_url,
+        options.token_url ??
+          DEFAULT_TOKEN_URL.replace('{universeDomain}', this.universeDomain),
         this.transporter,
         clientAuth
       );
@@ -197,10 +202,6 @@ export class ExternalAccountAuthorizedUserClient extends AuthClient {
         .eagerRefreshThresholdMillis as number;
     }
     this.forceRefreshOnFailure = !!additionalOptions?.forceRefreshOnFailure;
-
-    if (options.universe_domain) {
-      this.universeDomain = options.universe_domain;
-    }
   }
 
   async getAccessToken(): Promise<{
