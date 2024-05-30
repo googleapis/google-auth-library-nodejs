61e127bead3371d8204970b34592d01308a14a13
Jin <ustc.bigtailwolf@gmail.com>
2024-05-29 20:55:26 -0700

    Update src/auth/oauth2client.ts
    Co-authored-by: Daniel Bankhead <dan@danielbankhead.com>

--- src/auth/oauth2client.ts
+++ src/auth/oauth2client.ts
@@ -682,7 +682,7 @@ export class OAuth2Client extends AuthClient {
     const headers: Headers = {
       'Content-Type': 'application/x-www-form-urlencoded',
     };
-    const values = {
+    const values: Omit<GetTokenOptions, 'codeVerifier'> = {
       code: options.code,
       client_id: options.client_id || this._clientId,
       redirect_uri: options.redirect_uri || this.redirectUri,
